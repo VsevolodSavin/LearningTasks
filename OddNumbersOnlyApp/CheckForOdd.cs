c935250c832ca76e777ed35a9d08b7751734aee3
vsevolodsavin <savin.vsevolod@icloud.com>
2025-11-13 09:15:22 +0300

    Finds all odd numbers in the list

---
+++ OddNumbersOnlyApp/CheckForOdd.cs
@@ -0,0 +1,28 @@
+﻿
+//Написать метод, который фильтрует коллекцию чисел, возвращая только нечётные числа
+//public IEnumerable<int> GetOdd(ICollection<int> nums)
+
+namespace CheckForOdd;
+public class CheckForOdd
+{
+    public IEnumerable<int> GetOdd(ICollection<int> nums)
+    {
+        foreach (int num in nums)
+        {
+            if (num % 2 != 0) 
+                yield return num;
+        }
+    }
+
+    public static void Main()
+    {
+        var filter = new CheckForOdd();
+        var inputList = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        
+        foreach (var oddNum in filter.GetOdd(inputList))
+            Console.WriteLine(oddNum);
+    }
+}
+
+
+      
