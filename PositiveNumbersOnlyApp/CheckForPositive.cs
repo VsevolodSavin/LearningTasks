28aa62646f263e04d539b3e5df1e5472621f2337
vsevolodsavin <savin.vsevolod@icloud.com>
2025-11-14 09:27:33 +0300

    The application which revomes negative numbers from some collection and a Unit test for this application.

---
+++ PositiveNumbersOnlyApp/CheckForPositive.cs
@@ -0,0 +1,31 @@
+﻿//Написать метод, который фильтрует коллекцию чисел, возвращая только положительные числа
+//public ICollection<int> GetPositiveNumbers(ICollection<int> numbers)
+
+namespace PositiveNumbersOnlyApp
+{
+    public class CheckForPositive
+    {
+        public ICollection<int> GetPositiveNumbers(ICollection<int> numbers)
+        {
+            var positiveList = new List<int>();
+            foreach (int num in numbers)
+            {
+                if (num > 0)
+                    positiveList.Add(num);
+            }
+            return positiveList;
+        }
+        public static void Main(string[] args)
+        {
+            var checker = new CheckForPositive();
+            var allNumbers = new List<int> { -2, -1, 0, 1, 2, 3 };
+            
+            var positiveNumbers = checker.GetPositiveNumbers(allNumbers);
+
+            foreach (var num in positiveNumbers)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
---
+++ PositiveNumbersOnlyTest/PositiveNumbersUnitTest.cs
@@ -0,0 +1,66 @@
+﻿
+using PositiveNumbersOnlyApp;
+using Xunit;
+using Assert = Xunit.Assert;
+
+namespace PositiveNumbersOnlyTest;
+
+public class Tests
+{
+    //Возврат только положительных чисел
+    [Fact]
+    public void GetPositiveNumbers_StandardCase_ReturnsOnlyPositive()
+    {
+        // Arrange
+        var checker = new CheckForPositive();
+        var input = new List<int> { -2, -1, 0, 1, 2, 3 };
+
+        // Act
+        var result = checker.GetPositiveNumbers(input);
+
+        // Assert
+        Assert.Equal(new List<int> { 1, 2, 3 }, result);
+    }
+    //Только отрицательные значения
+    [Fact]
+    public void GetPositiveNumbers_AllNegative_ReturnsEmptyList()
+    {
+        // Arrange
+        var checker = new CheckForPositive();
+        var input = new List<int> { -5, -10, -1 };
+
+        // Act
+        var result = checker.GetPositiveNumbers(input);
+
+        // Assert
+        Assert.Empty(result);
+    }
+    //Пустой массив
+    [Fact]
+    public void GetPositiveNumbers_EmptyArray_ReturnsEmptyList()
+    {
+        // Arrange
+        var checker = new CheckForPositive();
+        var input = new List<int>();
+
+        // Act
+        var result = checker.GetPositiveNumbers(input);
+
+        // Assert
+        Assert.Empty(result);
+    }
+    //Граничные значения
+    [Fact]
+    public void GetPositiveNumbers_BoundaryValues_HandlesCorrectly()
+    {
+        // Arrange
+        var checker = new CheckForPositive();
+        var input = new List<int> { int.MinValue, -1, 0, 1, int.MaxValue };
+
+        // Act
+        var result = checker.GetPositiveNumbers(input);
+
+        // Assert
+        Assert.Equal(new List<int> { 1, int.MaxValue }, result);
+    }
+}
