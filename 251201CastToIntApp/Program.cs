1ebe4da5a054c4b833e17a661a1164c37534f6ce
vsevolodsavin <savin.vsevolod@icloud.com>
2025-12-01 09:18:05 +0300

    Cast int type to long

---
+++ 251201CastToIntApp/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+// скастить коллекцию интов в коллекцию лонгов
+// public ICollection<long> CastIntToLong(ICollection<int> list)
+
+namespace _251201CastToIntApp;
+
+public class CasterToLong
+{
+    public ICollection<long> CastIntToLong(ICollection<int> list)
+    {
+        return list
+            .Select(i => (long)i)
+            .ToList();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var caster = new CasterToLong();
+        var numbers = new List<int> { 10, 20, 30, 40, 50 };
+
+        var cast = caster.CastIntToLong(numbers);
+        Console.WriteLine(cast);
+    }
+}
