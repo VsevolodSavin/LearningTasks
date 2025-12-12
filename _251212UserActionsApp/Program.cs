0bb851ac770ebaded90baa1a66afc408cb603d99
vsevolodsavin <savin.vsevolod@icloud.com>
2025-12-12 09:31:19 +0300

    Different actions with users and collections App&Tests

---
+++ _251212UserActionsApp/Program.cs
@@ -0,0 +1,69 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace _251212UserActionsApp;
+
+public sealed record User
+{
+    public required Guid Id { get; init; }
+    public required int Age { get; init; }
+    public required string? Name { get; init; }
+}
+
+
+public sealed class Methods
+{ 
+    //Создать метод, который добавляет нового пользователя в конец коллекции.
+    //Использовать метод Append. Сравнить его работу с ручным добавлением через List.Add()
+    public ICollection<User> AddUserAtEnd(ICollection<User> users, User newUser)
+    {
+        return users
+            .Append(newUser)
+            .ToList();
+    }
+    
+    //Объединить две коллекции пользователей в одну
+    //Использовать метод Concat. Сравнить его работу с ручным объединением через AddRange()
+    public ICollection<User> MergeUserCollections(ICollection<User> firstGroup, ICollection<User> secondGroup)
+    {
+        return firstGroup
+            .Concat(secondGroup)
+            .ToList();
+    }
+    
+    //Создать метод, который добавляет нового пользователя в начало коллекции.
+    //Использовать метод Prepend. Сравнить его работу с ручной вставкой через Insert()
+    public ICollection<User> AddUserAtBeginning(ICollection<User> users, User newUser)
+    {
+        return users
+            .Prepend(newUser)
+            .ToList();
+    }
+
+    static void Main(string[] args)
+    {
+        IList<User> firstUserGroup = new List<User>
+        {
+            new User { Id = Guid.NewGuid(), Age = 25, Name = "Иван" },
+            new User { Id = Guid.NewGuid(), Age = 17, Name = "Андрей" },
+            new User { Id = Guid.NewGuid(), Age = 18, Name = "Анна" },
+            new User { Id = Guid.NewGuid(), Age = 20, Name = "Ирина" },
+            new User { Id = Guid.NewGuid(), Age = 95, Name = "Ирина" },
+            new User { Id = Guid.NewGuid(), Age = 25, Name = "Петр" }
+        };
+
+        var newUser = new User
+        {
+            Id = Guid.NewGuid(),
+            Age = 30,
+            Name = "Анатолий"
+        };
+
+        var method = new Methods();
+        var appendedUsers = method.AddUserAtEnd(firstUserGroup, newUser);
+
+        foreach (var user in appendedUsers)
+        {
+            Console.WriteLine($"  {user.Name}, {user.Age}");
+        }
+    }
+}
---
+++ _251212UserActionsTest/UnitTest1.cs
@@ -0,0 +1,150 @@
+﻿using _251212UserActionsApp;
+
+namespace _251212UserActionsTest
+{
+    public class MethodsTests
+    {
+        private readonly Methods _methods = new Methods();
+
+        [Fact]
+        public void AddUserAtEnd_ShouldAddUserToEndOfCollection()
+        {
+            // Arrange
+            var users = new List<User>
+            {
+                new User { Id = Guid.NewGuid(), Age = 25, Name = "Иван" },
+                new User { Id = Guid.NewGuid(), Age = 30, Name = "Мария" }
+            };
+            var newUser = new User { Id = Guid.NewGuid(), Age = 35, Name = "Алексей" };
+
+            // Act
+            var result = _methods.AddUserAtEnd(users, newUser);
+            var resultList = result.ToList();
+
+            // Assert
+            Assert.Equal(3, resultList.Count);
+            Assert.Equal(newUser, resultList.Last());
+            Assert.Equal(users[0], resultList[0]);
+            Assert.Equal(users[1], resultList[1]);
+        }
+        
+        [Fact]
+        public void AddUserAtEnd_WithEmptyCollection_ShouldReturnCollectionWithSingleUser()
+        {
+            // Arrange
+            var users = new List<User>();
+            var newUser = new User { Id = Guid.NewGuid(), Age = 35, Name = "Алексей" };
+
+            // Act
+            var result = _methods.AddUserAtEnd(users, newUser);
+            var resultList = result.ToList();
+
+            // Assert
+            Assert.Single(resultList);
+            Assert.Equal(newUser, resultList[0]);
+        }
+        
+        [Fact]
+        public void MergeUserCollections_ShouldCombineTwoCollections()
+        {
+            // Arrange
+            var firstGroup = new List<User>
+            {
+                new User { Id = Guid.NewGuid(), Age = 25, Name = "Иван" },
+                new User { Id = Guid.NewGuid(), Age = 30, Name = "Мария" }
+            };
+            var secondGroup = new List<User>
+            {
+                new User { Id = Guid.NewGuid(), Age = 35, Name = "Алексей" }
+            };
+
+            // Act
+            var result = _methods.MergeUserCollections(firstGroup, secondGroup);
+            var resultList = result.ToList();
+
+            // Assert
+            Assert.Equal(3, resultList.Count);
+            Assert.Equal(firstGroup[0], resultList[0]);
+            Assert.Equal(firstGroup[1], resultList[1]);
+            Assert.Equal(secondGroup[0], resultList[2]);
+        }
+
+        [Fact]
+        public void MergeUserCollections_WithEmptySecondCollection_ShouldReturnFirstCollection()
+        {
+            // Arrange
+            var firstGroup = new List<User>
+            {
+                new User { Id = Guid.NewGuid(), Age = 25, Name = "Иван" },
+                new User { Id = Guid.NewGuid(), Age = 30, Name = "Мария" }
+            };
+            var secondGroup = new List<User>();
+
+            // Act
+            var result = _methods.MergeUserCollections(firstGroup, secondGroup);
+            var resultList = result.ToList();
+
+            // Assert
+            Assert.Equal(2, resultList.Count);
+            Assert.Equal(firstGroup[0], resultList[0]);
+            Assert.Equal(firstGroup[1], resultList[1]);
+        }
+
+        [Fact]
+        public void MergeUserCollections_WithEmptyFirstCollection_ShouldReturnSecondCollection()
+        {
+            // Arrange
+            var firstGroup = new List<User>();
+            var secondGroup = new List<User>
+            {
+                new User { Id = Guid.NewGuid(), Age = 35, Name = "Алексей" }
+            };
+
+            // Act
+            var result = _methods.MergeUserCollections(firstGroup, secondGroup);
+            var resultList = result.ToList();
+
+            // Assert
+            Assert.Single(resultList);
+            Assert.Equal(secondGroup[0], resultList[0]);
+        }
+
+        [Fact]
+        public void AddUserAtBeginning_ShouldAddUserToBeginningOfCollection()
+        {
+            // Arrange
+            var users = new List<User>
+            {
+                new User { Id = Guid.NewGuid(), Age = 25, Name = "Иван" },
+                new User { Id = Guid.NewGuid(), Age = 30, Name = "Мария" }
+            };
+            var newUser = new User { Id = Guid.NewGuid(), Age = 35, Name = "Алексей" };
+
+            // Act
+            var result = _methods.AddUserAtBeginning(users, newUser);
+            var resultList = result.ToList();
+
+            // Assert
+            Assert.Equal(3, resultList.Count);
+            Assert.Equal(newUser, resultList.First());
+            Assert.Equal(users[0], resultList[1]);
+            Assert.Equal(users[1], resultList[2]);
+        }
+
+        [Fact]
+        public void AddUserAtBeginning_WithEmptyCollection_ShouldReturnCollectionWithSingleUser()
+        {
+            // Arrange
+            var users = new List<User>();
+            var newUser = new User { Id = Guid.NewGuid(), Age = 35, Name = "Алексей" };
+
+            // Act
+            var result = _methods.AddUserAtBeginning(users, newUser);
+            var resultList = result.ToList();
+
+            // Assert
+            Assert.Single(resultList);
+            Assert.Equal(newUser, resultList[0]);
+        }
+    }
+}
