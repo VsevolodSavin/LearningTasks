110f63941c003f813f888008aa1e0d4dad0b4628
vsevolodsavin <savin.vsevolod@icloud.com>
2025-12-16 00:07:15 +0300

    Aggregate App&Test

---
+++ _251216AggregateApp/Program.cs
@@ -0,0 +1,32 @@
+﻿// Написать метод сложения всех значений в массиве int[]. Не забыть про тесты. 
+
+namespace _251216AggregateApp;
+
+public sealed class Method
+{
+    public int? AggregateNumbers(ICollection<int>? numbers)
+    {
+        if (numbers == null || numbers.Count == 0)
+        {
+            return null;
+        }
+        return numbers
+            .Aggregate((x, y) => x + y);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var aggregator = new Method();
+            
+        int[] nums = { int.MaxValue, 1 };
+
+        var result = aggregator.AggregateNumbers(nums);
+
+        Console.WriteLine(int.MaxValue);
+        Console.WriteLine(int.MinValue);
+        Console.WriteLine($"Сумма: {string.Join(", ", result)}");
+    }
+}
---
+++ _251216AggregateTest/UnitTest1.cs
@@ -0,0 +1,78 @@
+﻿using _251216AggregateApp;
+
+namespace _251216AggregateTest;
+
+
+public class UnitTestForAggregate
+{
+    [Fact]
+    public void GetNumbersInArray_ReturnsSum()
+    {
+        //Arrange
+        var aggregator = new Method();
+        int[] nums = { 1, 2, 3, 4 };
+        
+        //Act
+        var result = aggregator.AggregateNumbers(nums);
+        
+        //Assert
+        Assert.Equal(10, result);
+    }
+    
+    [Fact]
+    public void GetNegatineNumbersInArray_ReturnsSum()
+    {
+        //Arrange
+        var aggregator = new Method();
+        int[] nums = { -1, -2, -3, -4 };
+        
+        //Act
+        var result = aggregator.AggregateNumbers(nums);
+        
+        //Assert
+        Assert.Equal(-10, result);
+    }
+    
+    [Fact]
+    public void GetZerosInArray_ReturnsSum()
+    {
+        //Arrange
+        var aggregator = new Method();
+        int[] nums = { 0, 0 };
+        
+        //Act
+        var result = aggregator.AggregateNumbers(nums);
+        
+        //Assert
+        Assert.Equal(0, result);
+    }
+    
+    [Fact]
+    public void GetEmptyArray_ReturnsNull()
+    {
+        //Arrange
+        var aggregator = new Method();
+        int[] nums = [];
+        
+        //Act
+        var result = aggregator.AggregateNumbers(nums);
+        
+        //Assert
+        Assert.Null(result);
+    }
+    
+    [Fact]
+    public void GetNullArray_ReturnsNull()
+    {
+        //Arrange
+        var aggregator = new Method();
+        int[] nums = null;
+        
+        //Act
+        var result = aggregator.AggregateNumbers(nums);
+        
+        //Assert
+        Assert.Null(result);
+    }
+    
+}
