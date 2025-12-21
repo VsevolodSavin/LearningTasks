229f5b2399051fcc6e40357c8c4039ecb2456369
vsevolodsavin <savin.vsevolod@icloud.com>
2025-12-21 22:33:14 +0300

    Checks if some sequances are equal App&Test

---
+++ _251222AreSequencesEqualApp/Program.cs
@@ -0,0 +1,39 @@
+﻿/* Нужно сравнить 2 списка значимого типа
+
+   var list = Enumerable.Range(0, 100).ToList();
+   var list1 = Enumerable.Range(0, 90).ToList();
+
+   //Пример вызова
+   bool result = list.AreSequencesEqual(list, list1);
+
+public bool AreSequencesEqual(
+   ICollection<int> firstList, 
+   ICollection<int> firstList)
+*/
+
+namespace _251222AreSequencesEqualApp;
+
+
+public sealed class Comparator
+{
+   public bool AreSequencesEqual(ICollection<int> firstList, ICollection<int> secondList)
+   {
+      return firstList.SequenceEqual(secondList);
+   }
+}
+
+
+class Program
+{
+   static void Main(string[] args)
+   {
+      var list = Enumerable.Range(0, 100).ToList();
+      var list1 = Enumerable.Range(0, 90).ToList();
+
+      var compare = new Comparator();
+      
+      bool result = compare.AreSequencesEqual(list, list1);
+
+      Console.WriteLine(result);
+   }
+}
---
+++ _251222AreSequencesEqualTest/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿
+using _251222AreSequencesEqualApp;
+
+namespace _251222AreSequencesEqualTest;
+
+public class UnitTest1
+{
+    [Fact]
+    public void GetEqualLists_ReturnTrue()
+    {
+        //Arrange
+        var compare = new Comparator();
+        var list = Enumerable.Range(0, 100).ToList();
+        var list1 = Enumerable.Range(0, 100).ToList();
+        
+        //Act
+        bool result = compare.AreSequencesEqual(list, list1);
+        
+        //Assert
+        Assert.True(result);
+    }
+    
+    [Fact]
+    public void GetDifferentLists_ReturnFalse()
+    {
+        //Arrange
+        var compare = new Comparator();
+        var list = Enumerable.Range(0, 100).ToList();
+        var list1 = Enumerable.Range(0, 90).ToList();
+        
+        //Act
+        bool result = compare.AreSequencesEqual(list, list1);
+        
+        //Assert
+        Assert.False(result);
+    }
+}
