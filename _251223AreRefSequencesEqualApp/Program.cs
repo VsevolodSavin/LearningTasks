0c3bc59c371514b7569c4a099153210503151bfa
vsevolodsavin <savin.vsevolod@icloud.com>
2025-12-23 00:57:58 +0300

    Check if some ref type sequences are equal App&Test

---
+++ _251223AreRefSequencesEqualApp/Program.cs
@@ -0,0 +1,67 @@
+﻿/* Нужно сравнить 2 списка ссылочного типа. 
+public sealed record User 
+   {
+       public required Guid Id { get; init; }
+       public required int Age { get; init; }
+   }
+   
+   public bool AreSequencesEqual(
+       ICollection<User> firstList, 
+       ICollection<User> firstList)
+
+*/
+
+
+namespace _251223AreRefSequencesEqualApp;
+
+
+
+public sealed record User 
+{
+    public required Guid Id { get; init; }
+    public required int Age { get; init; }
+}
+
+
+public sealed class Comparator
+{
+    public bool AreSequencesEqual(ICollection<User> firstList, ICollection<User> secondList)
+    {
+        return firstList.Select(u => u.Age)
+            .SequenceEqual(secondList.Select(u => u.Age));
+        //return firstList.Count() == secondList.Count()
+    }
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IList<User> firstList = new List<User>
+        {
+            new User { Id = Guid.NewGuid(), Age = 25},
+            new User { Id = Guid.NewGuid(), Age = 17},
+            new User { Id = Guid.NewGuid(), Age = 18},
+            new User { Id = Guid.NewGuid(), Age = 20},
+            new User { Id = Guid.NewGuid(), Age = 95},
+            new User { Id = Guid.NewGuid(), Age = 25}
+        }; 
+        
+        IList<User> secondList = new List<User>
+        {
+            new User { Id = Guid.NewGuid(), Age = 25},
+            new User { Id = Guid.NewGuid(), Age = 17},
+            new User { Id = Guid.NewGuid(), Age = 18},
+            new User { Id = Guid.NewGuid(), Age = 20},
+            new User { Id = Guid.NewGuid(), Age = 95},
+            new User { Id = Guid.NewGuid(), Age = 25}
+        };
+
+        var compare = new Comparator();
+
+        var result = compare.AreSequencesEqual(firstList, secondList);
+
+        Console.WriteLine(result);
+    }
+}
---
+++ _251223AreRefSequencesEqualTest/UnitTest1.cs
@@ -0,0 +1,73 @@
+﻿
+using _251223AreRefSequencesEqualApp;
+
+namespace _251223AreRefSequencesEqualTest;
+
+public class UnitTest1
+{
+    [Fact]
+    public void GetEqualLists_ReturnTrue()
+    {
+        //Arrange
+        var compare = new Comparator();
+        
+        IList<User> firstList = new List<User>
+        {
+            new User { Id = Guid.NewGuid(), Age = 25},
+            new User { Id = Guid.NewGuid(), Age = 17},
+            new User { Id = Guid.NewGuid(), Age = 18},
+            new User { Id = Guid.NewGuid(), Age = 20},
+            new User { Id = Guid.NewGuid(), Age = 95},
+            new User { Id = Guid.NewGuid(), Age = 25}
+        }; 
+        
+        IList<User> secondList = new List<User>
+        {
+            new User { Id = Guid.NewGuid(), Age = 25},
+            new User { Id = Guid.NewGuid(), Age = 17},
+            new User { Id = Guid.NewGuid(), Age = 18},
+            new User { Id = Guid.NewGuid(), Age = 20},
+            new User { Id = Guid.NewGuid(), Age = 95},
+            new User { Id = Guid.NewGuid(), Age = 25}
+        };
+        
+        //Act
+        var result = compare.AreSequencesEqual(firstList, secondList);
+        
+        //Assert
+        Assert.True(result);
+    }
+    
+    [Fact]
+    public void GetDifferentLists_ReturnFalse()
+    {
+        //Arrange
+        var compare = new Comparator();
+        
+        IList<User> firstList = new List<User>
+        {
+            new User { Id = Guid.NewGuid(), Age = 0},
+            new User { Id = Guid.NewGuid(), Age = 17},
+            new User { Id = Guid.NewGuid(), Age = 18},
+            new User { Id = Guid.NewGuid(), Age = 20},
+            new User { Id = Guid.NewGuid(), Age = 95},
+            new User { Id = Guid.NewGuid(), Age = 25}
+        }; 
+        
+        IList<User> secondList = new List<User>
+        {
+            new User { Id = Guid.NewGuid(), Age = 25},
+            new User { Id = Guid.NewGuid(), Age = 17},
+            new User { Id = Guid.NewGuid(), Age = 18},
+            new User { Id = Guid.NewGuid(), Age = 20},
+            new User { Id = Guid.NewGuid(), Age = 95},
+            new User { Id = Guid.NewGuid(), Age = 25}
+        };
+        
+        //Act
+        var result = compare.AreSequencesEqual(firstList, secondList);
+        
+        //Assert
+        Assert.False(result);
+    }
+}
