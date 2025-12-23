7ee44bb954c16844b22fbf6fff4447acde931216
vsevolodsavin <savin.vsevolod@icloud.com>
2025-12-24 01:17:57 +0300

    Check if ref class lists are equal App&Test

---
+++ _251224AreRefClassSequencesEqualApp/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace _251224AreRefClassSequencesEqualApp;
+
+
+public sealed class User 
+{
+    public required Guid Id { get; init; }
+    public required int Age { get; init; }
+}
+
+public sealed class Method
+{
+    public bool AreSequencesEqual(ICollection<User> firstList, ICollection<User> secondList)
+    {
+        return firstList.Select(u => u.Id)
+            .SequenceEqual(secondList.Select(u => u.Id));
+    }
+}
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var id1 = Guid.NewGuid();
+        var id2 = Guid.NewGuid();
+        
+        IList<User> firstList = new List<User>
+        {
+            new User { Id = id1, Age = 25},
+            new User { Id = id2, Age = 17}
+        }; 
+        
+        IList<User> secondList = new List<User>
+        {
+            new User { Id = id1, Age = 25},
+            new User { Id = id2, Age = 17}
+        };
+
+        var comparator = new Method();
+        var result = comparator.AreSequencesEqual(firstList, secondList);
+        Console.WriteLine(result);
+    }
+}
---
+++ _251224AreRefClassSequencesEqualTest/UnitTest1.cs
@@ -0,0 +1,63 @@
+﻿
+using _251224AreRefClassSequencesEqualApp;
+
+namespace _251224AreRefClassSequencesEqualTest;
+
+public class UnitTest1
+{
+    [Fact]
+    public void GetEqualLists_ReturnTrue()
+    {
+        //Arrange
+        var comparator = new Method();
+        
+        var id1 = Guid.NewGuid();
+        var id2 = Guid.NewGuid();
+        
+        IList<User> firstList = new List<User>
+        {
+            new User { Id = id1, Age = 25},
+            new User { Id = id2, Age = 17}
+        }; 
+        
+        IList<User> secondList = new List<User>
+        {
+            new User { Id = id1, Age = 25},
+            new User { Id = id2, Age = 17}
+        };
+        
+        //Act
+        var result = comparator.AreSequencesEqual(firstList, secondList);
+        
+        //Assert
+        Assert.True(result);
+    }
+    
+    [Fact]
+    public void GetDifferentLists_ReturnFalse()
+    {
+        //Arrange
+        var comparator = new Method();
+        
+        var id1 = Guid.NewGuid();
+        var id2 = Guid.NewGuid();
+        
+        IList<User> firstList = new List<User>
+        {
+            new User { Id = id1, Age = 25},
+            new User { Id = id1, Age = 17}
+        }; 
+        
+        IList<User> secondList = new List<User>
+        {
+            new User { Id = id2, Age = 25},
+            new User { Id = id2, Age = 17}
+        };
+        
+        //Act
+        var result = comparator.AreSequencesEqual(firstList, secondList);
+        
+        //Assert
+        Assert.False(result);
+    }
+}
